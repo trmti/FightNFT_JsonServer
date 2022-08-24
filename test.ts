d179cc5fb654d8c0b5e54e1a707f8388360a6c9e
trmti <trmtikko@gmail.com>
2022-08-24 14:58:47 +0900

    return json file

--- test.ts
+++ test.ts
@@ -1,3 +1,17 @@
 import { serve } from 'https://deno.land/std@0.145.0/http/server.ts';
 
-serve((req: Request) => new Response('Hello World'));
+serve((req: Request) => {
+  const url = new URL(req.url);
+  if (url.pathname !== '/nft') {
+    return new Response('Not Found', { status: 404 });
+  }
+  if (req.method !== 'GET') {
+    return new Response('Bad Request', { status: 400 });
+  }
+  return Response.json({
+    name: 'test',
+    description: 'sample text',
+    image: 'https://i.imgur.com/TySZlZ5.jpg',
+    attributes: {},
+  });
+});
